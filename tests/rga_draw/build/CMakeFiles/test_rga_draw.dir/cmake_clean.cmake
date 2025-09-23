a0017569e2333280570a50c7679a25d099d57110
Tz-slayer <q1554788327@gmail.com>
2025-09-23 02:21:24 +0000

    修复test_rga_draw.cpp中如果外部终止导致析构函数无法执行

---
+++ tests/rga_draw/build/CMakeFiles/test_rga_draw.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/BasicDrawStrategy.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/DMAPool.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/GC9A01Display.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/OrangePi3BGPIO.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/OrangePi3BSPI.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/RGAImageProcessor.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/home/orangepi/newbot/rk3566_gc9a01/utils/PerformanceTester.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/test_rga_draw.cpp.o"
+  "CMakeFiles/test_rga_draw.dir/usr/local/src/dma_alloc.cpp.o"
+  "test_rga_draw"
+  "test_rga_draw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_rga_draw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
